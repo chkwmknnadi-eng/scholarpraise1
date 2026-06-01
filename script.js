8b87b9fe26306c39b09f89badc429274cc8682f1
chkwmknnadi-eng <chkwmknnadi@gmail.com>
2026-06-01 14:36:14 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const messageBtn = document.getElementById('messageBtn');
+
+if (messageBtn) {
+    messageBtn.addEventListener('click', () => {
+        alert('Thank you for your interest! Please reach out via email or phone.');
+    });
+}
